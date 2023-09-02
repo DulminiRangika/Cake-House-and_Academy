87d163a6c01cb478a28741c3e3b003a2984b497a
D&D CAKE HOUSE & ACADEMY <143689516+DulminiRangika@users.noreply.github.com>
2023-09-02 15:51:08 +0530

    Add files via upload

---
+++ First javascript file.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").addEventListener("click", function() {
+  window.location.href = "second html file.html";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+  window.location.href = "Cupcakes html.html";
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+  window.location.href = "Birthday cakes file.html";
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+  window.location.href = "Wedding cakes html file.html";
+});
+
+document.getElementById("button5").addEventListener("click", function() {
+  window.location.href = "Other html file.html";
+});
